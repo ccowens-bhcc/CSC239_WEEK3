e111e4ab564cec7d92fb39364671098011b1832b
Chris Owens <christopher.owens01@bhcc.edu>
2025-09-18 11:24:27 -0400

    Thursday update to penguin.java

--- GitDemoProject/src/Penguin.java
+++ GitDemoProject/src/Penguin.java
@@ -1,6 +1,6 @@
 public class Penguin {
     static int version = 1;
     static void sayHello() {
-        System.out.println("Hi, I'm Class Penguin, version " + version + "!");
+        System.out.println("Hi, I'm not really a Penguin, version " + version + "!");
     }
 }
