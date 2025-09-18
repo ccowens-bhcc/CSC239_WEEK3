aad64fdb8120116ba671f682df1fa9b964915eb5
Chris Owens <christopher.owens01@bhcc.edu>
2025-09-18 10:04:45 -0400

    Committing Version 3.0

--- GitDemoProject/src/Giraffe.java
+++ GitDemoProject/src/Giraffe.java
@@ -1,5 +1,5 @@
 public class Giraffe {
-    static int version = 1
+    static int version = 2
     ;
     static void sayHello() {
         System.out.println("Hi, I'm Class Giraffe, version " + version + "!");
