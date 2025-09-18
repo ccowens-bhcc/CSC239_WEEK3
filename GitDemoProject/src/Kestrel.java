31758bc070661ccc71f370003eccd2d8c7631c70
Chris Owens <christopher.owens01@bhcc.edu>
2025-09-18 10:03:49 -0400

    Committing v2.0

--- GitDemoProject/src/Kestrel.java
+++ GitDemoProject/src/Kestrel.java
@@ -1,5 +1,5 @@
 public class Kestrel {
-    static int version = 0;
+    static int version = 1;
     static void sayHello() {
         System.out.println("Hi, I'm Class Kestrel, version " + version + "!");
     }
--- GitDemoProject/src/Penguin.java
+++ GitDemoProject/src/Penguin.java
@@ -1,5 +1,5 @@
 public class Penguin {
-    static int version = 0;
+    static int version = 1;
     static void sayHello() {
         System.out.println("Hi, I'm Class Penguin, version " + version + "!");
     }
