8242f1fc7997b572927e004d0004ed572b94ef6d
Percival Villalva <70678259+PerVillalva@users.noreply.github.com>
2025-09-18 11:45:24 -0400

    Upgrade each class version by 1 and create a new class Dog

--- GitDemoProject/src/Penguin.java
+++ GitDemoProject/src/Penguin.java
@@ -1,5 +1,5 @@
 public class Penguin {
-    static int version = 1;
+    static int version = 2;
     static void sayHello() {
         System.out.println("Hi, I'm Class Penguin, version " + version + "!");
     }
